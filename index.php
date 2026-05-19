85c7fd26f91c9aa162bb50c78ebecda0a9df6119
Shainamarie <modestoshaina54@gmail.com>
2026-05-19 14:13:54 +0800

    create HTML Header

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>My Portfolio</title>
+
+</head>
+
+<body>
+
+  
+  <nav>
+    <div class="logo">
+      MY PORTFOLIO
+    </div>
+
+    <ul>
+      <li><a href="#about">About</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+
+  
+  
